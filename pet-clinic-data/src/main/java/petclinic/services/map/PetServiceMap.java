24cfae8630c8c2e5436700b8b7b1330cfde61695
Federico Cao <federico.cao94@gmail.com>
2019-09-16 23:51:14 +0200

    Update map to manage ID closes #24

--- pet-clinic-data/src/main/java/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/petclinic/services/map/PetServiceMap.java
@@ -12,7 +12,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 
 	@Override
 	public Pet save(Pet object) {
-		return super.save(object.getId(), object);
+		return super.save(object);
 	}
 
 	@Override
@@ -23,11 +23,6 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 	@Override
 	public Pet findById(Long id) {
 		return super.findById(id);
-	}
-
-	@Override
-	public Pet save(Long id, Pet object) {
-		return super.save(id, object);
 	}
 
 	@Override
