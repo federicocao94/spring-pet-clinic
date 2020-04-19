bd66f3699b4f238e3722de513135b14c930bf168
federicocao <federico.cao94@gmail.com>
2020-04-19 22:57:33 +0200

    Vets list json endpoint closes #67

--- pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import petclinic.model.Vet;
 import petclinic.services.VetService;
 
 @Controller
@@ -17,8 +22,16 @@ public class VetController {
 
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
 	public String listVets(Model model) {
+		
 		model.addAttribute("vets", vetService.findAll());
 		
 		return "vets/index";
+	}
+	
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		
+		return vetService.findAll();
 	}
 }
